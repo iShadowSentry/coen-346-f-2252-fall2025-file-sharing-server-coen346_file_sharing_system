787a00692471b0e7dd8dd7c8eff286a428ffe870
iShadowSentry <hanishpagadala05@gmail.com>
2025-11-08 17:28:28 -0500

    Test#1

--- FileServer/src/main/java/ca/concordia/Main.java
+++ FileServer/src/main/java/ca/concordia/Main.java
@@ -4,7 +4,8 @@ import ca.concordia.server.FileServer;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
+        System.out.println("Hello");
 
         FileServer server = new FileServer(12345, "filesystem.dat", 10 * 128);
         // Start the file server
