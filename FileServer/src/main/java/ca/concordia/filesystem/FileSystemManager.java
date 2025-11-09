9b7be1b245db7dd8a33fe370a82ab3adad2fd57a
Justin Pasztor <114451072+joostainn@users.noreply.github.com>
2025-11-08 19:03:37 -0500

    MetaData-section variables and related private functions

--- FileServer/src/main/java/ca/concordia/filesystem/FileSystemManager.java
+++ FileServer/src/main/java/ca/concordia/filesystem/FileSystemManager.java
@@ -1,7 +1,9 @@
 package ca.concordia.filesystem;
 
 import ca.concordia.filesystem.datastructures.FEntry;
+import ca.concordia.filesystem.datastructures.FNode;
 
+import javax.swing.*;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
@@ -10,6 +12,13 @@ import java.util.Arrays;
 
 public class FileSystemManager {
 
+    // ============================= PRIVATE VARIABLES ============================= //
+
+    private FEntry[] fentryTable;
+    private FNode[] fnodesTable;
+    private FEntry[] inodeTable; // Array of inodes
+    private boolean[] freeBlockList; // Bitmap for free blocks
+
     private final int MAXFILES = 5;
     private final int MAXBLOCKS = 10;
     //private static FileSystemManager instance;
@@ -17,20 +26,74 @@ public class FileSystemManager {
     private final ReentrantLock globalLock = new ReentrantLock();
 
     private static final int BLOCK_SIZE = 128; // Example block size
+    private static final int FENTRY_BYTES = 15; // number of bytes for an FEntry
+    private static final int FNODE_BYTES = 4; // number of bytes for an FNode
 
-    private FEntry[] inodeTable; // Array of inodes
-    private boolean[] freeBlockList; // Bitmap for free blocks
+    private int fentryRegionBytes; //calculate total fentry bytes
+    private int fnodeRegionBytes; // calculate total fnode bytes
+    private int metadataBytes; // total metadata bytes
+    private int firstDataBlockIndex; //offset to the first data block
+
+
+    // ============================= PRIVATE FUNCTIONS ============================= //
+    private void computeMetaDataBytes(){
+        fentryRegionBytes = MAXFILES*FENTRY_BYTES;
+        fnodeRegionBytes = MAXBLOCKS*FNODE_BYTES;
+        metadataBytes = fentryRegionBytes + fnodeRegionBytes;
+        firstDataBlockIndex = (metadataBytes + BLOCK_SIZE - 1)/BLOCK_SIZE; // first data block index
+    }
+
+    private int findFile(String name){
+        for (int i = 0; i < MAXFILES; i++) {
+            if(fentryTable[i] != null && fentryTable[i].getFilename().equals(name)) return i;
+        }
+        return -1;
+    }
+    private int findFreeSlot(){
+        for (int i = 0; i < MAXFILES; i++) {
+            if(fentryTable[i] == null) return i;
+        }
+        return -1;
+    }
+
+    private int countAvailableBlocks(){
+        int count = 0;
+        for (boolean f:freeBlockList) if(f) count++;
+        return count;
+    }
+
 
     public FileSystemManager(String filename, int totalSize) {
         // Initialize the file system manager with a file
         try{
             disk = new RandomAccessFile(filename, "rw");
             disk.setLength(totalSize);
+            computeMetaDataBytes();
+            System.out.println("First data block index: " + firstDataBlockIndex);
             System.out.println("Disk created with size: " + totalSize + " bytes");
+            fentryTable = new FEntry[MAXFILES];
+            fnodesTable = new FNode[MAXBLOCKS];
+            freeBlockList = new boolean[MAXBLOCKS];
+
+            for (int i = 0; i < MAXBLOCKS; i++) {
+                freeBlockList[i] = true; //all blocks are free at the start
+            }
+
+            for (int i = 0; i < firstDataBlockIndex; i++) {
+                freeBlockList[i] = false; // index up until first datablock is reserved for metadata
+            }
+
+
         } catch (IOException e){
             System.out.println("Disk could not be created.");
             e.printStackTrace();
         }
+
+
+
+
+
+
 //        if(instance == null) {
 //
 //            //TODO Initialize the file system
@@ -44,6 +107,8 @@ public class FileSystemManager {
         FileSystemManager newFile = new FileSystemManager(fileName, 5);
         // TODO
         //throw new UnsupportedOperationException("Method not implemented yet.");
+
+
     }
 
 
